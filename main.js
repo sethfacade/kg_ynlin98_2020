e2020f34c0fb84bdd3e7c4fad8f2d3080bb7e6f7
Yan Naing Lin <ynlin98@gmail.com>
2021-04-29 12:20:05 -0400

    Created a phoneticInteger function with a dictionary called integerPhonetic that represents phonetic equivalent from 0-9

--- main.js
+++ main.js
@@ -4,3 +4,18 @@
 // For example:
 // Given an array: [3, 25, 209], print “Three,TwoFive,TwoZeroNine” into stdout.
 // Given an array: [10, 300, 5], print “OneZero,ThreeZeroZero,Five” into stdout.
+
+function phoneticInteger(arrOfNum) {
+  const integerPhonetic = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine",
+  };
+}
